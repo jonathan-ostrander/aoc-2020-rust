7c97a4be785946a4aeb1a4fbc89ad585d3e98cd7
Jonathan Ostrander <jonathanost@gmail.com>
2020-12-01 10:09:38 -0500

    ugly day one

--- src/bin/dayone.rs
+++ src/bin/dayone.rs
@@ -1,5 +1,26 @@
 use std::fs;
+use std::collections::HashSet;
 
 fn main() {
-  let contents = fs:
+  let contents = fs::read_to_string("src/input/dayone.txt")
+    .expect("error reading the file");
+  let lines = contents.lines();
+  let mut input = HashSet::new();
+  for line in lines {
+    input.insert(line.trim().parse::<i64>().expect("unable to parse int"));
+  }
+  for x in input.iter() {
+    if input.contains(&(2020 - x)) {
+      println!("Part One: {}", x * (2020 - x));
+      break;
+    }
+  }
+  for x in input.iter() {
+    for y in input.iter() {
+      if input.contains(&(2020 - x - y)) {
+        println!("Part Two: {}", x * y * (2020 - x - y));
+        break;
+      }
+    }
+  }
 }
